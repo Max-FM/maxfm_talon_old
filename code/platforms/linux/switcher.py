cf18073c18fd52caa701ca56173b3ea6a00219b7
Max Foxley-Marrable <max.foxley-marrable@port.ac.uk>
2022-04-05 17:12:33 +0100

    Remove redundant code

--- code/platforms/linux/switcher.py
+++ code/platforms/linux/switcher.py
@@ -12,6 +12,3 @@ class SwitcherActions:
     def switcher_menu():
         """Open a menu of running apps to switch to"""
         actions.key('super-s') # Only tested in Ubuntu.
-    # def switcher_launch(path: str):
-    #     """Launch a new application by path (all OSes), or AppUserModel_ID path on Windows"""
-    #     subprocess.call(path)
