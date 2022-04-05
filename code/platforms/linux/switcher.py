3baaa99d6ae23553e242d89d63ad530eaea9c65e
Max Foxley-Marrable <max.foxley-marrable@port.ac.uk>
2022-04-05 17:10:52 +0100

    Linux override of switcher_menu

---
+++ code/platforms/linux/switcher.py
@@ -0,0 +1,17 @@
+from talon import Context, actions, ui
+import subprocess
+
+ctx = Context()
+
+ctx.matches = """
+os: linux
+"""
+
+@ctx.action_class("user")
+class SwitcherActions:
+    def switcher_menu():
+        """Open a menu of running apps to switch to"""
+        actions.key('super-s') # Only tested in Ubuntu.
+    # 'def switcher_launch(path: str):
+    #     """Launch a new application by path (all OSes), or AppUserModel_ID path on Windows"""
+    #     subprocess.call(path)'
