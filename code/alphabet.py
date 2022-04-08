7e84a57f893da86f0778e184763973e6396f63c6
Max Foxley-Marrable <max.foxley-marrable@port.ac.uk>
2022-04-08 17:23:55 +0100

    Update alphabet

--- code/alphabet.py
+++ code/alphabet.py
@@ -1,7 +1,7 @@
 from talon import Context
 ctx = Context()
 
-# TODO: The "help alphabet" menu does not update to show the new alphabet and instead shows the original alphabet. Need to find a workaround. 
+# TODO: The "help alphabet" menu does not update to show the new alphabet and instead shows the original alphabet. Need to find a workaround.
 
 # Need to make the file context more specific than knausj_talon\code\keys.py file in order to successfully override.
 ctx.matches = r"""
@@ -11,7 +11,7 @@ os: linux
 """
 
 # default_alphabet = "air bat cap drum each fine gust harp sit jury crunch look made near odd pit quench red sun trap urge vest whale plex yank zip".split(" ")
-new_alphabet = "air bat cap drum each fine gust heart ink jury crunch look made near oval pit quench red sun trap urge vest whale plex yank zip".split(" ")
+new_alphabet = "air bat cap drum each fine gust heart ink jury crunch look made near ox pit quench red sun trap urge vest whale plex yank zip".split(" ")
 letters_string = "abcdefghijklmnopqrstuvwxyz"
 alphabet = dict(zip(new_alphabet, letters_string))
 ctx.lists["self.letter"] = alphabet
