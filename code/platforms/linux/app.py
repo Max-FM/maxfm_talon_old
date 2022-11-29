7a89502c5441d19aeb4a90ddfa5123defc339c09
Max Foxley-Marrable <22856173+Max-FM@users.noreply.github.com>
2022-11-29 09:42:45 -0500

    Add tiling and workspace management commands

--- code/platforms/linux/app.py
+++ code/platforms/linux/app.py
@@ -21,3 +21,18 @@ class AppActions:
     def window_split_right():
         """Maximize the window to the right side of the screen."""
         actions.key("super-ctrl-right")
+    def window_tiling():
+        """Toggle Pop_OS! window tiling."""
+        actions.key("super-y")
+    def workspace_next():
+        """Switch to the next workspace."""
+        actions.key("super-ctrl-down")
+    def workspace_previous():
+        """Switch to the previous workspace."""
+        actions.key("super-ctrl-up")
+    def move_window_previous_workspace():
+        """Move window to the previous workspace."""
+        actions.key("super-shift-up")
+    def move_window_next_workspace():
+        """Move window to the next workspace."""
+        actions.key("super-shift-down")
